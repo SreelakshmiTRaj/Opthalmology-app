34eb6a2e10f1c83ea156e92b52e632cff77a6bbf
Sreelakshmi T Raj <sreelakshmithirithiyil@gmail.com>
2025-11-04 15:49:08 +0530

    Committing with left side bubbles

--- src/app/components/Body/Body.js
+++ src/app/components/Body/Body.js
@@ -62,39 +62,14 @@ export default function Body() {
 
         {/* RIGHT SIDE LABELS */}
         {[
-          { text: "Neurology", top: "85px", left: "935px", width: "110px" },
-          {
-            text: "Cardiovascular",
-            top: "160px",
-            left: "990px",
-            width: "147px",
-          },
-          { text: "Dermatology", top: "243px", left: "980px", width: "130px" },
-          {
-            text: "Radiation Oncology",
-            top: "325px",
-            left: "960px",
-            width: "181px",
-          },
-          {
-            text: "Skeletal System",
-            top: "407px",
-            left: "975px",
-            width: "154px",
-          },
-          {
-            text: "Women’s Health",
-            top: "475px",
-            left: "960px",
-            width: "155px",
-          },
-          {
-            text: "Emerging Viruses",
-            top: "550px",
-            left: "950px",
-            width: "167px",
-          },
-          { text: "Genetics", top: "615px", left: "920px", width: "100px" },
+          {text: "Neurology", top: "85px", left: "935px", width: "110px" },
+          {text: "Cardiovascular",top: "160px",left: "990px",width: "147px",},
+          {text: "Dermatology", top: "243px", left: "980px", width: "130px" },
+          {text: "Radiation Oncology",top: "325px",left: "960px",width: "181px",},
+          {text: "Skeletal System",top: "407px",left: "975px",width: "154px",},
+          {text: "Women’s Health",top: "475px",left: "960px",width: "155px",},
+          {text: "Emerging Viruses",top: "550px",left: "950px",width: "167px",},
+          {text: "Genetics", top: "615px", left: "920px", width: "100px" },
         ].map((label, idx) => (
           <div
             key={`right-${idx}`}
@@ -113,18 +88,8 @@ export default function Body() {
 
         {/* LEFT SIDE LABELS */}
         {[
-          {
-            text: "Endocrinology",
-            top: "120px",
-            left: "600px",
-            width: "142px",
-          },
-          {
-            text: "Gastroenterology",
-            top: "185px",
-            left: "530px",
-            width: "166px",
-          },
+          {text: "Endocrinology",top: "120px",left: "600px",width: "142px",},
+          {text: "Gastroenterology", top: "185px",left: "530px", width: "166px",},
           { text: "Geriatrics", top: "265px", left: "600px", width: "106px" },
           { text: "Hepatology", top: "340px", left: "555px", width: "120px" },
           { text: "Inflammation", top: "420px", left: "560px", width: "132px" },
